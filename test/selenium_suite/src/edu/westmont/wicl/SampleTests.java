32edbd8687e26033262d076ca6a1b8a83463df28
Adrian Humphrey <adrianhumphrey@Adrians-MacBook-Air.local>
2016-11-29 15:50:35 -0800

    Test for clinking About Button.

--- test/selenium_suite/src/edu/westmont/wicl/SampleTests.java
+++ test/selenium_suite/src/edu/westmont/wicl/SampleTests.java
@@ -38,8 +38,7 @@ public class SampleTests {
 		if(testStaging){
 			addresses.add("http://djp3.westmont.edu/ally_staging/ally/");
 		}
-		
-		System.setProperty("webdriver.chrome.driver", "/Users/djp3/git/sbfoodbank/test/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "../chromedriver");
         // Create a new instance of the Google driver
         // Notice that the remainder of the code relies on the interface, 
         // not the implementation.
@@ -57,6 +56,28 @@ public class SampleTests {
 
 	@After
 	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testAboutButton(){
+		//open web page
+		if(testLocal){
+			driver.get("http://localhost:8080");
+		}
+		else{
+			//Test Production code about button press
+			driver.get("http://djp3.westmont.edu/ally/ally/");
+		}
+		
+		//set the driver for the about button
+		WebElement element = driver.findElement(By.name("About_Button"));
+		
+		//Click About button
+		element.click();
+
+		
+		// Check the title of the page
+        assertTrue(driver.getTitle().equals("Ally - About"));
 	}
 	
 	@Test
