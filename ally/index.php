e4d54a8eca52394fa02b6b94d716eaf568eb0d8c
Don Patterson <d_j_p_3@djp3.net>
2016-12-06 14:24:50 -0800

    switching links to php not html

--- ally/index.php
+++ ally/index.php
@@ -21,7 +21,7 @@
 		<div class="container text-center">
 			<div class="row-fluid">
 				<div class="col-md-12">
-					<a class="btn btn-default btn-lg" href="find-food.html" role="button" name="find_food_btn">
+					<a class="btn btn-default btn-lg" href="find-food.php" role="button" name="find_food_btn">
 						<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
 						Find Food
 					</a>
@@ -59,7 +59,7 @@
 			</div>
 			<div class="row-fluid">
 				<div class="col-lg-12">
-					<a class="btn btn-default btn-lg" href="about.html" role="button" name="aboutbutton">
+					<a class="btn btn-default btn-lg" href="about.php" role="button" name="aboutbutton">
 						<span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span>
 						About
 						</a>
