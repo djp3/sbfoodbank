841dbc69cc91183efd2b475b111d363eb6581328
Don Patterson <d_j_p_3@djp3.net>
2016-12-06 15:54:30 -0800

    Remove the Google cheese query

--- test/selenium_suite/src/edu/westmont/wicl/SampleTests.java
+++ test/selenium_suite/src/edu/westmont/wicl/SampleTests.java
@@ -100,36 +100,7 @@ public class SampleTests {
 		}
 	}
 
-	@Test
-	public void testGoogleCheeseQuery() {
-        // And now use this to visit Google
-        driver.get("http://www.google.com");
-        
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
-        
-        // Check the title of the page
-        assertTrue(driver.getTitle().equals("Google"));
-
-        // Find the text input element by its name
-        WebElement element = driver.findElement(By.name("q"));
-
-        // Enter something to search for
-        element.sendKeys("Cheese!");
-
-        // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
-
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith("cheese!");
-            }
-        });
-        
-        // Check the title of the page
-        assertTrue(driver.getTitle().startsWith("Cheese! - Google"));
-	}
+	
 	
 
 	@Test
