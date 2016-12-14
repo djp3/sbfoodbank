7c654f49dbc73ed85a95ecdbbf605f3ae1ac7fb6
Don Patterson <d_j_p_3@djp3.net>
2016-12-13 17:08:48 -0800

    Removed a bad test file from Jacobs branch and broke out the selenium tests into a different class for each page.

---
+++ test/selenium_suite/src/edu/westmont/wicl/System_Tests.java
@@ -0,0 +1,100 @@
+package edu.westmont.wicl;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class System_Tests {
+	
+	static TestState state;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		state = new TestState();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		state.shutdown();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testSystemUp() {
+		
+		//Test that the system is up and that all the static variables are correct
+		for(String address:state.addresses){
+			Map<String,String> pagesToTest = new HashMap<String,String>();
+			pagesToTest.putAll(state.allpages);
+
+			for (Entry<String, String> e : pagesToTest.entrySet()) {
+				state.driver.get(address + e.getKey());
+
+				try{
+					// Check the title of the page
+					assertTrue(state.driver.getTitle().equals(e.getValue()));
+				}
+				catch(AssertionError ex){
+					System.err.print("Failed on this page:"+e.toString());
+					throw(ex);
+				}
+					
+					
+			}
+		}
+	}
+	
+
+	@Test
+	public void test_ButtonHome() {
+		for (String address : state.addresses) {
+			Map<String,String> pagesToTest = new HashMap<String,String>();
+			pagesToTest.putAll(state.allpages);
+			pagesToTest.remove("/index.php"); // The home page doesn't have a home button
+
+			for (Entry<String, String> e : pagesToTest.entrySet()) {
+				state.driver.get(address + e.getKey());
+
+				// Check the title of the page
+				assertTrue(state.driver.getTitle().equals(e.getValue()));
+
+				// Find the home button by its name
+				WebElement element = state.driver.findElement(By.id("home_button"));
+
+				// Click the found button
+				element.click();
+
+				// Wait for the page to load, timeout after 10 seconds
+				(new WebDriverWait(state.driver, 10)).until(new ExpectedCondition<Boolean>() {
+					public Boolean apply(WebDriver d) {
+						return d.getTitle().toLowerCase().startsWith("ally");
+					}
+				});
+
+				// Check the title of the page
+				assertTrue(state.driver.getTitle().equals("Ally - Santa Barbara FoodBank"));
+			}
+		}
+	}
+}
---
+++ test/selenium_suite/src/edu/westmont/wicl/TestState.java
@@ -0,0 +1,54 @@
+package edu.westmont.wicl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestState {
+	public Boolean testProduction = false;
+	public Boolean testStaging = false;
+	public Boolean testLocal = true;
+	public WebDriver driver;
+	public List<String> addresses = new ArrayList<String>();
+	public Map<String, String> allpages = new HashMap<String, String>();
+	
+	TestState(){
+		if (testLocal) {
+			addresses.add("http://localhost:8080");
+		}
+		if (testProduction) {
+			addresses.add("http://djp3.westmont.edu/ally/ally");
+		}
+		if (testStaging) {
+			addresses.add("http://djp3.westmont.edu/ally_staging/ally");
+		}
+
+		if (System.getProperty("os.name").contains("indows")) {
+			System.setProperty("webdriver.chrome.driver", "../chromedriver.exe");
+		} else {
+			System.setProperty("webdriver.chrome.driver", "../chromedriver");
+		}
+
+		allpages.put("/about.php", "Ally - About");
+		allpages.put("/feedback.php", "Ally - Feedback");
+		allpages.put("/find-food.php", "Ally - Find Food");
+		allpages.put("/find-other.php", "Ally - Find Other Resources");
+		allpages.put("/index.php", "Ally - Santa Barbara FoodBank");
+		allpages.put("/map-many.php", "Ally - Partner Locator");
+		allpages.put("/map-one.php", "Ally - Partner Locator");
+		allpages.put("/share.php", "Ally - Share");
+
+		// Create a new instance of the Google driver
+		// Notice that the remainder of the code relies on the interface,
+		// not the implementation.
+		driver = new ChromeDriver();
+	}
+
+	public void shutdown() {
+		driver.quit();
+	}
+}
